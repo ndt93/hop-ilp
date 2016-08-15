d1f09a52ec37f7596463ebdf3176b67cd5802c2e
ndt <thiennguyen1993@gmail.com>
2016-08-15 22:29:56 +0800

    Get turn data from server

--- hop-ilp/src/client/__init__.py
+++ hop-ilp/src/client/__init__.py
@@ -5,10 +5,32 @@ from logger import logger
 
 
 class Client(object):
+    buffer = bytearray() # Left-over data from the previous recv
 
     def __init__(self, client_name, hostname="localhost", port=2323):
         self.client_name = client_name
         self.sock = self.connect(hostname, port)
+
+    def receive_turn(self):
+        resp = self.receive_response(end_tags=['</turn>', '</round-end>', '</session-end>'])
+
+        if 'turn' in resp:
+            return 'turn', resp['turn']
+        elif 'round-end' in resp:
+            return 'round', resp['round-end']
+        else:
+            return 'session', resp['session-end']
+
+    def request_round(self):
+        req_data = {
+            'round-request': None
+        }
+        self.send_request(req_data)
+        return self.receive_round_init()
+
+    def receive_round_init(self):
+        resp = self.receive_response(end_tags=['</round-init>'])
+        return resp['round-init']
 
     def request_session(self, inst_name):
         req_data = {
@@ -18,37 +40,44 @@ class Client(object):
                 'no-header': None
             },
         }
+        self.send_request(req_data)
+        return self.receive_session_init()
+
+    def receive_session_init(self):
+        resp = self.receive_response(end_tags=['</session-init>'])
+        return resp['session-init']
+
+    def send_request(self, req_data):
         bin_data = bytearray(xmltodict.unparse(req_data),
                              encoding='utf-8')
         bin_data.append(0)
         self.sock.sendall(bin_data)
-        return self.receive_session_init()
 
-    def receive_session_init(self):
-        resp = self.receive_response(end_tag="</session-init>")
-        return {
-            'session_id': resp['session-id'],
-            'num_rounds': resp['num-rounds'],
-            'time-allowed': resp['time-allowed'],
-        }
+    def receive_response(self, end_tags):
+        all_data = self.loop_receive(end_tags)
+        xmlstr = all_data.decode()
+        return xmltodict.parse(xmlstr)
 
-    def receive_response(self, end_tag):
+    def loop_receive(self, end_tags):
         all_data = bytearray()
-        data = self.sock.recv(4096)
+
+        if len(self.buffer) > 0:
+            data = bytes(self.buffer)
+        else:
+            data = self.sock.recv(4096)
 
         while data:
             all_data.extend(data)
 
-            if data[-1] == '\0':
-                all_data = all_data[:-1]
-                break
-            if all_data[-len(end_tag):].decode() == end_tag:
-                break
+            for end_tag in end_tags:
+                start_index = all_data.find(end_tag)
+                if start_index >= 0:
+                    end_index = start_index + len(end_tag)
+                    self.buffer = bytearray(all_data[end_index:])
+                    all_data = all_data[:end_index]
+                    return all_data
 
             data = self.sock.recv(4096)
-
-        xmlstr = all_data.decode()
-        return xmltodict.parse(xmlstr)
 
     def connect(self, hostname, port):
         sock = socket.socket()
--- hop-ilp/src/experiment/rddl.py
+++ hop-ilp/src/experiment/rddl.py
@@ -1,5 +1,6 @@
 from base import Experiment
 from client import Client
+from logger import logger
 
 
 class RDDLExperiment(Experiment):
@@ -7,5 +8,31 @@ class RDDLExperiment(Experiment):
     session_info = {}
 
     def start(self):
+        self.solver.solve()
         self.client = Client('python-client')
         self.session_info = self.client.request_session(self.solver.problem_name)
+        logger.info('session_start|session={}'.format(self.session_info));
+
+        session_end_info = self.loop_rounds()
+        logger.info('session_end|info={}'.format(session_end_info))
+
+    def loop_rounds(self):
+        while True:
+            round_info = self.client.request_round()
+            logger.info('round_start|round={}'.format(round_info));
+
+            t, end_info = self.loop_turns()
+            if t != 'round':
+                return end_info
+            logger.info('round_end|info={}'.format(end_info))
+
+    def loop_turns(self):
+        while True:
+            t, data = self.client.receive_turn()
+            if t != 'turn':
+                return t, data
+
+            states = self.get_states_from_turn(data)
+
+    def get_states_from_turn(self, turn):
+        pass
